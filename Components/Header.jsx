871fffcb7b32a6ff6014c81d5dd0ed8187704255
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-07-03 04:13:29 -0700

    feat: styled and finished the Header component

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -6,7 +6,21 @@ const Header = () => {
     return (
         <div className="py-5 px-5 md:px-5 lg:px-28">
             <div className="flex justify-between items-center">
-                <Image src={assets.logo} width={180} alt="Logo" className="w-[130px] sm:w-auto" />
+                <Image src={assets.logo} width={62} height={60} alt="Logo" className="w-[130px] sm:w-auto" />
+                <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]">
+                    Get Started 
+                    <Image src={assets.arrow} width={20} height={20} alt="Arrow" />
+                </button>
+            </div>
+            <div className="text-center my-8">
+                <h1 className="text-3xl sm:text-5xl font-medium">Hottest Gist</h1>
+                <p className="mt-10 max-w-[720px] m-auto text-xs sm:text-base">Hey there, Gist Lover! 🌟 Welcome to Da Gist Blog, where the juiciest stories and hottest trends collide! Whether you're here for the latest celebrity buzz, fashion tips, or tech updates, we've got the inside scoop just for you. Buckle up, because things are about to get spicy! Let's dive into the world of Gist together. 🎉</p>
+                <form className="flex justify-between max-w-[500px] scale-72 sm:scale-100 mx-auto mt-10 border border-black shadow-[-8px_8px_0px_#000000]" 
+                action="">
+                    <input type="email" placeholder="Enter Email" className="pl-4 outline-none"/>
+                    <button type='submit' className="border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active::text-white">Subscribe</button>
+
+                </form>
             </div>
         </div>
     );
