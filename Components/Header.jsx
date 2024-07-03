57cd66d3fc084ccc97c3a16a4b14d7fdbba35593
Rabiu Muhammad Nasir <111989601+DarkVayder@users.noreply.github.com>
2024-07-02 17:26:04 -0700

    feat: debugged header error

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,16 +1,15 @@
-import React from "react"
+import React from "react";
 import { assets } from "@/assets/assets"; 
-import image from "next/image"
+import Image from "next/image";
 
 const Header = () => {
     return (
-        <div className="py-5 px-5 md:px lg:px-28">
+        <div className="py-5 px-5 md:px-5 lg:px-28">
             <div className="flex justify-between items-center">
-                <image src = {assets.logo} width={180} alt="" className="w-[130px] sm-w-auto" />
-
+                <Image src={assets.logo} width={180} alt="Logo" className="w-[130px] sm:w-auto" />
             </div>
         </div>
-    )
+    );
 }
 
-export default Header;  
+export default Header;
